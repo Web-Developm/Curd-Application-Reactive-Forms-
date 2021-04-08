d2193e6c362a1ff5cf2ae3f548b3eab979cf296c
Satyakrishnajasti <satyajasti9@gmail.com>
2021-04-08 12:43:44 +0530

    update

--- src/app/conditional-reactive/conditional-reactive.component.ts
+++ src/app/conditional-reactive/conditional-reactive.component.ts
@@ -1,6 +1,6 @@
 import { variable } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-conditional-reactive',
@@ -12,7 +12,8 @@ export class ConditionalReactiveComponent implements OnInit {
   constructor() { }
 
   data = new FormGroup({
-    fname: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    member: new FormControl('', [Validators.required]),
+    fname: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(20)]),
     lname: new FormControl('', [Validators.required, Validators.minLength(5)]),
     user: new FormControl('', [Validators.required, Validators.minLength(4)]),
     email: new FormControl('', [Validators.required, Validators.minLength(10)]),
@@ -23,6 +24,33 @@ export class ConditionalReactiveComponent implements OnInit {
     phone: new FormControl('', [Validators.required, Validators.minLength(10)]),
     address: new FormControl('', [Validators.required, Validators.minLength(10)])
   });
+
+  public store: Array<{ member: any; fname: any; lname: any; user: any; email: any; password: any; cpassword: any; date: any; gender: any; phone: any; address: any }> = [];
+
+  add = (): any => {
+    this.store.push({
+      member: this.data.controls['member'].value,
+      fname: this.data.controls['fname'].value,
+      lname: this.data.controls['lname'].value,
+      user: this.data.controls['user'].value,
+      email: this.data.controls['email'].value,
+      password: this.data.controls['password'].value,
+      cpassword: this.data.controls['cpassword'].value,
+      date: this.data.controls['date'].value,
+      gender: this.data.controls['gender'].value,
+      phone: this.data.controls['phone'].value,
+      address: this.data.controls['address'].value
+    });
+  }
+
+
+  category = (): void => {
+    let user = this.data.get('user');
+    let phone = this.data.get('phone');
+    let address = this.data.get('address');
+  }
+
+
 
   match = (): any => {
     if (this.data.controls['password'].value == this.data.controls['cpassword'].value) {
@@ -61,7 +89,7 @@ export class ConditionalReactiveComponent implements OnInit {
 
   }
 
-  onSubmit=():void =>{
+  onSubmit = (): void => {
     console.log(this.data.value);
   }
 
